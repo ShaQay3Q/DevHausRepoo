39ef7fb58266fa1cb1b59b0a15a3427833a156dd
ShaQay3Q <shaghay3gh@gmail.com>
2024-05-10 12:07:15 +0200

    assignment in the class - 10.04.2024 - finding the largest number in an array

---
+++ JS/solved-asignments/largest-number.js
@@ -0,0 +1,20 @@
+/*==================================
+    LARGEST NUMBER ALGORITHM
+==================================*/
+
+function findLargestNumber(arrNumber){
+    let largest = 0
+    if (arrNumber.length === 0){
+        return console.log(`the list is empty`);
+    }
+    for (let i = 0; i < arrNumber.length; i++){
+        if (arrNumber[i] > largest){
+            largest = arrNumber[i];
+        }
+    }
+    return largest;
+}
+
+const arrlist = [1, 5, 87, 2, 5, 76, 98, 100, 2, 54, 7];
+
+console.log(findLargestNumber(arrlist));
