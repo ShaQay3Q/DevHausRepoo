2feb7c81389d32421e2ab61d0ae4cee35d0bd3aa
ShaQay3Q <shaghay3gh@gmail.com>
2024-05-10 16:27:10 +0200

    data restructuring - ex_03.js

---
+++ JS/solved-asignments/ex_03.js
@@ -0,0 +1,15 @@
+/**
+Exercise 3
+Rewrite the code below to use array destructuring instead of assigning each value to a variable.
+*/
+console.log("EXERCISE 3");
+
+let person = [12, "Chris", "Owen"];
+
+// let firstName = person[1];
+// let lastName = person[2];
+// let age = person[0];
+
+let [age, firstName, lastName] = person;
+
+console.log(`Person - Age: ${age}, Name: ${firstName} ${lastName}`);
