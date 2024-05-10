29c0f588cf262c563e38e48abb67b5c7883e77c8
ShaQay3Q <shaghay3gh@gmail.com>
2024-05-10 16:24:41 +0200

    data restructuring - ex_02.js

---
+++ JS/solved-asignments/ex_02.js
@@ -0,0 +1,16 @@
+/**
+Exercise 2
+Rewrite the code below to assign each number to the right variable.
+*/
+console.log("EXERCISE 2");
+
+let numbers = [3, 5, 4, 2, 6, 1];
+
+// let [one, two, three, four, five, six] = numbers;
+
+let [three, five, four, two, six, one] = numbers;
+
+
+console.log(
+  `One: ${one}, Two: ${two}, Three: ${three}, Four: ${four}, Five: ${five}, Six: ${six}`
+);
