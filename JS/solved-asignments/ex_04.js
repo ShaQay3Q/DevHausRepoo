1cc14b94d7afd343bc93df30c6d269b2b0ff030a
ShaQay3Q <shaghay3gh@gmail.com>
2024-05-10 16:31:02 +0200

    data restructuring - ex_04.js

---
+++ JS/solved-asignments/ex_04.js
@@ -0,0 +1,16 @@
+/**
+Exercise 4
+Rewrite the code below to use array destructuring instead of assigning each value to a variable.
+Make sure not to have unused variables.
+Hint: https://untangled.io/in-depth-es6-destructuring-with-assembled-avengers
+*/
+console.log("EXERCISE 4");
+
+let person = ["Chris", 12, "Owen"];
+
+// let firstName = person[0];
+// let lastName = person[2];
+
+let [firstName, age, lastName] = person;
+
+console.log(`Name: ${firstName} ${lastName}`);
