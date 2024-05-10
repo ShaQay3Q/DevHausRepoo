b2636674a0719ac9c99c0b6d38fd266436595a57
ShaQay3Q <shaghay3gh@gmail.com>
2024-05-10 16:37:24 +0200

    data restructuring - ex_06.js

---
+++ JS/solved-asignments/ex_06.js
@@ -0,0 +1,13 @@
+/**
+Exercise 6
+Using Array Destructuring get all of the names from this Nested Array
+Hint: https://untangled.io/in-depth-es6-destructuring-with-assembled-avengers
+*/
+console.log("EXERCISE 6");
+
+const moreStudents = ["Chris", ["Ahmad", "Antigoni"], ["Toby", "Sam"]];
+
+// Write your code here
+const [student1, [student2, student3], [student4, student5]] = moreStudents;
+
+console.log(student1, student2, student3, student4, student5);
