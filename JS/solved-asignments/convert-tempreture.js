f5b211bc795542db85cc4bcf7c73fd9c69d9e7f9
ShaQay3Q <shaghay3gh@gmail.com>
2024-05-13 10:11:14 +0200

    c to f, vise versa converter - problems solved

--- JS/solved-asignments/convert-tempreture.js
+++ JS/solved-asignments/convert-tempreture.js
@@ -10,7 +10,7 @@ function temperatureConverter01(degree, unit){
         degree = (degree * 9/5) + 32;
         
     } else if (unit === "fahrenheit" || unit === "f"){
-        degree = (degree - 32) * 5/9;
+        degree = Math.round((degree - 32) * 5/9);
     } else {
         return console.log("enter either celsius or c, or fahrenheit or f");
     }
@@ -20,22 +20,24 @@ function temperatureConverter01(degree, unit){
 temperatureConverter01(0, "f");
 
 function fahrenheitToCelsiusConv(degree){
-    return degree = (32*degree * 9/5) + 32;
+    return Math.round((degree - 32) * 5/9);
 }
 
 function celsiusToFahrenheitConv(degree){
-    return degree = (degree - 32) * 5/9;
+    return (degree * 9/5) + 32;
 }
 
 function temperatureConverter02(degree, unit){
     switch (unit){
+        case "fahrenheit":
         case "f":
-            return fahrenheitToCelsiusConv(degree); 
+            return console.log(`${fahrenheitToCelsiusConv(degree)} ${unit}`);
+        case "celsius":
         case "c":
-            return celsiusToFahrenheitConv(degree);
+            return console.log(`${celsiusToFahrenheitConv(degree)} ${unit}`); 
         default:
             return "enter either celsius or c, or fahrenheit or f";
     }
 }
 
-console.log(`${temperatureConverter02(0, "f")}`);
+temperatureConverter02(76, "c");
