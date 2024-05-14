2d86e75a8bea8ed4c05a0c382d23ae47353629dc
ShaQay3Q <shaghay3gh@gmail.com>
2024-05-14 16:39:44 +0200

    more code was added. new methids

--- JS/DOM-basics/page.js
+++ JS/DOM-basics/page.js
@@ -26,12 +26,23 @@ decrement.addEventListener("click", () => {
     
 })
 
-increment.addEventListener("click", () => {
-    count++;
-    countElement.innerText = count;
+increment.addEventListener("click", (event) => {
+    //if you hold shiftkey when clicking add 10 to count
+    // if (event.shiftKey === true)
+    // if (event.shiftKey){
+    //     count += 10;
+    // } else {
+    //     count++;
+    // }
+    //     countElement.innerText = count;
+
+
+    //turnery expression
+    count += event.shiftKey ? 10 : 1
+
 })
 
 reset.addEventListener("click", () => {
     count = 0;
     countElement.innerText = count;
-})f
+})
