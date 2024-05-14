a0fc63f0903d813ece01244d581cf46474029080
ShaQay3Q <shaghay3gh@gmail.com>
2024-05-14 15:06:38 +0200

    I wanna cry

---
+++ JS/DOM-basics/page.js
@@ -0,0 +1,22 @@
+const countElement = document.getElementById("count");
+console.log(countElement);
+
+const [decrement, increment, reset] = document.getElementsByTagName("button");
+
+let count = 0;
+
+decrement.addEventListener("click", () => {
+    count--;
+    countElement.innerText = count;
+})
+
+increment.addEventListener("click", () => {
+    count++;
+    countElement.innerText = count;
+})
+
+reset.addEventListener("click", () => {
+    count = 0;
+    countElement.innerText = count;
+
+})
